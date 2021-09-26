5f614b6f5ac754629719fd1dd2ddfcc1eeb8070e
tminhat13 <nmtran@cpp.edu>
2021-09-25 23:53:17 -0700

    update naieve

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,26 +1,48 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
+# AUTHOR: Nhat Tran
 # FILENAME: title of the source file
 # SPECIFICATION: description of the program
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 1
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
 
 #importing some Python libraries
+import csv
+
 from sklearn.naive_bayes import GaussianNB
 
+dbTraining = []
 #reading the training data
-#--> add your Python code here
+with open('weather_training.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for i, row in enumerate(reader):
+        if i > 0:  # skipping the header
+            dbTraining.append(row)
 
 #transform the original training features to numbers and add to the 4D array X. For instance Sunny = 1, Overcast = 2, Rain = 3, so X = [[3, 1, 1, 2], [1, 3, 2, 2], ...]]
 #--> add your Python code here
-# X =
+num_attributes = 4
+dim1, dim2 = (num_attributes, len(dbTraining))
+X = [[0 for i in range(dim1)] for j in range(dim2)]
+for k in range(len(dbTraining)):
+    for j in range(len(dbTraining[k]) - 1):
+        if (dbTraining[k][j] == "Sunny") | (dbTraining[k][j] == "Hot") | (dbTraining[k][j] == "High") | (dbTraining[k][j] == "Weak"):
+            X[k][j] = 1
+        elif (dbTraining[k][j] == "Overcast") | (dbTraining[k][j] == "Mild") | (dbTraining[k][j] == "Normal") | (dbTraining[k][j] == "Strong"):
+            X[k][j] = 2
+        elif (dbTraining[k][j] == "Rain") | (dbTraining[k][j] == "Cold"):
+            X[k][j] = 3
 
 #transform the original training classes to numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
 #--> add your Python code here
-# Y =
+Y = ["0"] * len(dbTraining)
+for k in range(len(dbTraining)):
+    if dbTraining[k][4] == "No":
+        Y[k] = 2
+    elif dbTraining[k][4] == "Yes":
+        Y[k] = 1
 
 #fitting the naive bayes to the data
 clf = GaussianNB()
